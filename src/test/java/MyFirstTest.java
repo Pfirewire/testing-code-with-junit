9de0a957ea11067513afe2670179103d72798a8c
Stephen Pflug <stephen@pflug.us>
2022-08-31 12:36:41 -0500

    test commit

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -45,4 +45,5 @@ public class MyFirstTest {
         assertTrue(language.contains("H")); // use assertTrue for this statement
         assertFalse(language.contains("J")); // use assertFalse for this statement
     }
+    // testing
 }
